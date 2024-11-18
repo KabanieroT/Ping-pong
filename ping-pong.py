1803515b58c6ddc107aeb61f0b08b780a15d9bba
KabanieroT <andrew.t.maltsev@gmail.com>
2024-11-18 20:02:56 +0300

    Update ping-pong.py ver.1 Beta 2
    Добавил мячик, и возможность мячика бегать по полю

--- ping-pong.py
+++ ping-pong.py
@@ -31,27 +31,39 @@ win_width = 700
 win_height = 700
 window = display.set_mode((win_width, win_height))
 display.set_caption("Ping_pong")
-field = (120, 255, 120)
+field = (180, 214, 214)
 game = True
 fps = 60
 clock = time.Clock()
 raketka_left = Player('raketka_left.png', 10, 350, 10, 16, 64)
 raketka_right = Player('raketka_right.png', 674, 350, 10, 16, 64)
-# ball = GameSprite()
+ball = GameSprite('ball.png', 350, 350, 10, 32, 32)
+ball_speed_x = randint(-2, 2)
+ball_speed_y = randint(-2, 2)
+if ball_speed_x == 0:
+    ball_speed_x = 1
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     window.fill(field)
+    ball.rect.x += ball_speed_x
+    ball.rect.y += ball_speed_y
+    if ball.rect.y > 668 or ball.rect.y < 0:
+        ball_speed_y *= -1
+    if sprite.collide_rect(raketka_left, ball):
+        ball_speed_x = randint(0, 5)
+        ball_speed_y = randint(-5, 5)
+    elif sprite.collide_rect(raketka_right, ball):
+        ball_speed_x = randint(-5, 0)
+        ball_speed_y = randint(-5, 5)
+    ball.reset()
     raketka_right.update_right()
     raketka_right.reset()
     raketka_left.update_left()
     raketka_left.reset()
     display.update()
     clock.tick(fps)
-    
-
-
     
 
 
