d03f198fdaa8f8dc99067b21ea880c0d567fd8dc
KabanieroT <andrew.t.maltsev@gmail.com>
2024-11-25 19:27:38 +0300

    Update ping-pong.py ver.1
    Добавил плохую, но остановку игры. Первая полноценная версия проекта, несмотря на недоработки и лень автора
    
    
    ヾ(•ω•`)o

--- ping-pong.py
+++ ping-pong.py
@@ -38,6 +38,7 @@ bg_colors = [randint(0, 255), randint(0, 255), randint(0, 255)]
 bg_colors_fin = [randint(0, 255), randint(0, 255), randint(0, 255)]
 field = (bg_colors[0], bg_colors[1], bg_colors[2])
 game = True
+finish = False
 fps = 60
 clock = time.Clock()
 
@@ -55,48 +56,61 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    if bg_colors[0] != bg_colors_fin[0]:
-        if bg_colors[0] > bg_colors_fin[0]:
-            bg_colors[0] = max(bg_colors[0] - 1, 0)  # Убедитесь, что значение не меньше 0
+    if not finish:
+        if bg_colors[0] != bg_colors_fin[0]:
+            if bg_colors[0] > bg_colors_fin[0]:
+                bg_colors[0] = max(bg_colors[0] - 1, 0)  # Убедитесь, что значение не меньше 0
+            else:
+                bg_colors[0] = min(bg_colors[0] + 1, 255)  # Убедитесь, что значение не больше 255
+        if bg_colors[1] != bg_colors_fin[1]:
+            if bg_colors[1] > bg_colors_fin[1]:
+                bg_colors[1] = max(bg_colors[1] - 1, 0)
+            else:
+                bg_colors[1] = min(bg_colors[1] + 1, 255)
+        if bg_colors[2] != bg_colors_fin[2]:
+            if bg_colors[2] > bg_colors_fin[2]:
+                bg_colors[2] = max(bg_colors[2] - 1, 0)
+            else:
+                bg_colors[2] = min(bg_colors[2] + 1, 255)
         else:
-            bg_colors[0] = min(bg_colors[0] + 1, 255)  # Убедитесь, что значение не больше 255
-    if bg_colors[1] != bg_colors_fin[1]:
-        if bg_colors[1] > bg_colors_fin[1]:
-            bg_colors[1] = max(bg_colors[1] - 1, 0)
-        else:
-            bg_colors[1] = min(bg_colors[1] + 1, 255)
-    if bg_colors[2] != bg_colors_fin[2]:
-        if bg_colors[2] > bg_colors_fin[2]:
-            bg_colors[2] = max(bg_colors[2] - 1, 0)
-        else:
-            bg_colors[2] = min(bg_colors[2] + 1, 255)
-    else:
-        bg_colors_fin = [randint(0, 255), randint(0, 255), randint(0, 255)]
-    field = (bg_colors[0], bg_colors[1], bg_colors[2])
-    window.fill(field)
-    window.blit(font.render(str(count_win[0]) + ' - ' + str(count_win[1]), True, (0, 0, 0)), (325, 30))
-    ball.rect.x += ball_speed_x
-    ball.rect.y += ball_speed_y
-    if ball.rect.y > 668 or ball.rect.y < 0:
-        ball_speed_y *= -1
-    if sprite.collide_rect(raketka_left, ball):
-        ball_speed_x = abs(ball_speed_x)+1
-        ball_speed_y = randint(0-abs(ball_speed_x), abs(ball_speed_x))
-    elif sprite.collide_rect(raketka_right, ball):
-        ball_speed_x = 0 - ball_speed_x - 1
-        ball_speed_y = randint(0-abs(ball_speed_x), abs(ball_speed_x))
-    if ball.rect.x > 668 or ball.rect.x < 0:
-        if ball.rect.x > 668:
-            count_win[0] += 1
-        else:
-            count_win[1] += 1
-        ball.rect.x = 350
-        ball.rect.y = 350
-        ball_speed_y = randint(-2, 2)
-        if randint(0, 1) == 0:
-            ball_speed_x = 2
-        else:
-            ball_speed_x = -2
+            bg_colors_fin = [randint(0, 255), randint(0, 255), randint(0, 255)]
+        field = (bg_colors[0], bg_colors[1], bg_colors[2])
+        window.fill(field)
+        window.blit(font.render(str(count_win[0]) + ' - ' + str(count_win[1]), True, (0, 0, 0)), (325, 30))
+        ball.rect.x += ball_speed_x
+        ball.rect.y += ball_speed_y
+        if ball.rect.y > 668 or ball.rect.y < 0:
+            ball_speed_y *= -1
+        if sprite.collide_rect(raketka_left, ball):
+            ball_speed_x = abs(ball_speed_x)+1
+            ball_speed_y = randint(0-abs(ball_speed_x), abs(ball_speed_x))
+        elif sprite.collide_rect(raketka_right, ball):
+            ball_speed_x = 0 - ball_speed_x - 1
+            ball_speed_y = randint(0-abs(ball_speed_x), abs(ball_speed_x))
+        if ball.rect.x > 668 or ball.rect.x < 0:
+            if ball.rect.x > 668:
+                count_win[0] += 1
+            else:
+                count_win[1] += 1
+            ball.rect.x = 350
+            ball.rect.y = 350
+            ball_speed_y = randint(-2, 2)
+            if randint(0, 1) == 0:
+                ball_speed_x = 2
+            else:
+                ball_speed_x = -2
+        if count_win[0] == 5:
+            text_surface = font.render("Выиграл левый", True, (255, 255, 255))
+            window.blit(text_surface, (100, 200))
+            finish = True
+        elif count_win[1] == 5:
+            text_surface = font.render("Выиграл правый", True, (255, 255, 255))
+            window.blit(text_surface, (350, 200))
+            finish = True
+
+
+
+
 
     ball.reset()
     raketka_right.update_right()
@@ -107,3 +121,5 @@ while game:
     clock.tick(fps)
 
 print(bg_colors)
+
+    
